49decb8224aa8f39154e70d06a6860fadaeacf73
Vadim <wadim_fo@mail.ru>
2023-02-13 01:12:15 +0300

    Homeworks 13.02

---
+++ Homeworks/Homework_1/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача № 1
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine ("Необходимо из двух чисел найти и вывести большее и меньшее число");
+// Console.WriteLine ("Введите первое число");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите второе число");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// int max = numA;
+// int min = 1;
+
+// if ( numA > numB ) 
+// {
+//     max = numA;
+//     min = numB;
+// }
+// else 
+// {
+//     max = numB;
+//     min = numA;
+// }
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+// Console.Write("min = ");
+// Console.WriteLine(min);
+
+// Задача № 1 выполнена
+
+
+
+// Задача № 2
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine ("Необходимо определить большее из 3х чисел");
+// Console.WriteLine ("Введите первое число");
+// int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите второе число");
+// int numB = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Введите третье число");
+// int numC = Convert.ToInt32(Console.ReadLine());
+// int max = numA;
+
+// if (numA>max) max = numA;
+// if (numB>max) max = numB;
+// if (numC>max) max = numC;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+// Задача № 2 выполнена
+
+
+
+// Задача № 3
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine ("Необходимо определить, является ли число четным");
+// Console.WriteLine ("Введите число");
+// int numN = Convert.ToInt32(Console.ReadLine());
+
+// if (numN%2==0)
+// Console.WriteLine("Четное");
+// else 
+// Console.WriteLine("Нечетное");
+
+// Задача № 3 выполнена
+
+
+
+// Задача № 4
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. 
+
+// Console.WriteLine ("Необходимо найти все чётные числа от 1 до N.");
+// Console.WriteLine ("Введите число N");
+// int numN = Convert.ToInt32(Console.ReadLine());
+// int current = 1;
+
+// while(current <= numN)
+// {
+//     if(current%2==0)
+//     Console.Write(current + " ");      
+//     current++;
+// }
+// Console.WriteLine($"- Все четные числа содержащиейся от 1 до {numN}"); 
+
+// Задача № 4 выполнена
