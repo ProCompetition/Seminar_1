86f632b276963ea0c101a24d075cb80ae110e49a
Vadim <wadim_fo@mail.ru>
2023-02-15 20:52:54 +0300

    Добавление семинара №2

---
+++ Seminars/Seminar_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное число из отрезкка [10, 99] и показывает наибольшую цифру числа.
+
+void MaxDecimal (int num)
+{
+    int ed = num % 10;
+    int dec = num / 10;
+    if(ed>dec)
+    {
+        Console.WriteLine($"Большая цифра числа {num} -> {ed}");
+    }
+    else
+    {
+        Console.WriteLine($"Большая цифра числа {num} -> {dec}");
+    }
+}
+
+int randomnumber = new Random().Next(10, 100);
+
+MaxDecimal(randomnumber);
