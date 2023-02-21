8702510d702d65aeaccd0fb0e9b6d4ff42e8e4d0
Vadim <wadim_fo@mail.ru>
2023-02-21 17:01:49 +0300

    решена вторая задача

--- Homeworks/Homework_3/Program.cs
+++ Homeworks/Homework_3/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задача № 1
 // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.WriteLine ("Задача на определение палиндрома, необходимо ввести число");
-Console.WriteLine ("Введите пятизначное число");
-int numA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine ("Задача на определение палиндрома, необходимо ввести число");
+// Console.WriteLine ("Введите пятизначное число");
+// int numA = Convert.ToInt32(Console.ReadLine());
 
 
 
@@ -21,8 +21,29 @@ int numA = Convert.ToInt32(Console.ReadLine());
 // Задача № 2
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-// необходимо добавить ya, ybк задаче с семинара
+// необходимо добавить ya, yb к задаче с семинара
 
+// double FindDistance (double xa, double ya, double xb, double yb, double za, double zb)
+// {
+//     return Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb-ya, 2) + Math.Pow(zb-za, 2));
+// }
+
+// Console.WriteLine("Input coordinate x for first point:");
+// double xa = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input coordinate x for second point:");
+// double xb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input coordinate y for first point:");
+// double ya = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input coordinate y for second point:");
+// double yb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input coordinate z for first point:");
+// double za = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input coordinate z for second point:");
+// double zb = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Distance between our points -> {FindDistance(xa,ya,za,xb,yb,zb)}");
+
+// Задача № 2 решена по примеру задачи с семинара с добавлением точки в третьем векторе измерения
 
 
 
