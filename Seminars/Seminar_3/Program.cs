f1b806056319cc829d17e9605ad9285e164fb1b9
Vadim <wadim_fo@mail.ru>
2023-02-21 16:26:16 +0300

    делали семинар за 17.02

---
+++ Seminars/Seminar_3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача №1 Напишите программу, которая принимает на вход точки (X и Y)
+
+// int FindQuart (double x, double y)
+// {
+//     if(x>0 && y>0) return 1;        // указываем четверти осей координат
+//     if(x<0 && y>0) return 2;
+//     if(x<0 && y<0) return 3;
+//     if(x>0 && y<0) return 4;
+//     return 0;
+// }
+
+
+// Console.WriteLine("Input X:");      // ввод данных
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input Y:");
+// int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(FindQuart (x, y));    // получение резултата
+
+
+
+// Задача № 2 Напишите программу, которая принимает координаты двух точек и находит расстояние между ними в 2д пространстве
+
+// double FindDistance (double xa, double ya, double xb, double yb)
+// {
+//     return Math.Sqrt(Math.Pow(xb-xa,2) + (Math.Pow(yb-ya,2)));
+// }
+
+// Console.WriteLine("Input coordinate x for first point:");
+// double xa = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input coordinate y for first point:");
+// double ya = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input coordinate x for second point:");
+// double xb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input coordinate y for second point:");
+// double yb = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Distance between our points -> {FindDistance(xa,ya,xb,yb)}");
+
