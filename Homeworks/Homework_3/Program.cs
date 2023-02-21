ebaa7106fe25fbd7b7f2f0799b5c5eab57bbadb7
Vadim <wadim_fo@mail.ru>
2023-02-21 21:56:48 +0300

    Update homework 3

--- Homeworks/Homework_3/Program.cs
+++ Homeworks/Homework_3/Program.cs
@@ -54,5 +54,43 @@
 
 // Задача № 3
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine ("Задача, необходимо написать программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+Console.WriteLine ("Введите число - N");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("\n Задача 23");
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
 
