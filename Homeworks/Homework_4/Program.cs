e537a45d2aa824e6cddc19e87dafb5b95bcb45be
CT <ct@CTs-MacBook-Air.local>
2023-02-22 22:05:39 +0300

    Дополненная домашняя работа №4

--- Homeworks/Homework_4/Program.cs
+++ Homeworks/Homework_4/Program.cs
@@ -21,9 +21,50 @@
 // 82 -> 10
 // 9012 -> 12
 
+// Console.WriteLine("Задача, необходимо ввести число, в результате получим сумму цифр этого числа");
+// Console.WriteLine("Введите число"); 
+// int num = int.Parse(Console.ReadLine());
+//             int sum = 0;
+//             while (num > 0)
+//             {
+//                 sum = sum + num % 10;
+//                 num = num /10 ; 
+//             }
+// Console.WriteLine($"Сумма цифр в заданном числе = {sum}");
+
 
 
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+// int [] CreateRandomArray (int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input maximum value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+
+// ShowArray(myArray);
