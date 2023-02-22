5d0d046186fcf80c41dc49651a878d17478cd0df
CT <ct@CTs-MacBook-Air.local>
2023-02-22 16:54:35 +0300

    Дополнение к домашнему заданию номер 3

--- Homeworks/Homework_3/Program.cs
+++ Homeworks/Homework_3/Program.cs
@@ -61,27 +61,19 @@ Console.WriteLine ("Задача, необходимо написать прог
 Console.WriteLine ("Введите число - N");
 int Num = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
-
-Console.WriteLine("\n Задача 23");
-Console.Write("Введите число: ");
-int cube = Convert.ToInt32(Console.ReadLine());
-
-void Cube(int[] cube){
-  int counter = 0;
-  int length = cube.Length;
-  while (counter <  length){
-    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+void Cube(int[] Num)
+{
+  int counter = 1;
+  int length = Num.Length;
+  while (counter <  length)
+  {
+    Num[counter] = Convert.ToInt32(Math.Pow(counter, 3));
     counter++;
   }
 }
 
-void PrintArry(int[] coll){
+void PrintArry(int[] coll)
+{
   int count = coll.Length;
   int index = 0;
   while(index < count){
@@ -90,7 +82,7 @@ void PrintArry(int[] coll){
   }
 } 
 
-int[] arry = new int[cube+1];
+int[] arry = new int[Num+1];
 Cube(arry);
 PrintArry(arry);
 
